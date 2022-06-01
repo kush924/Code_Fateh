e614763803c08c1d51b25e2b6acddafc9c0b245a
kush924 <kush924@gmail.com>
2022-06-02 04:37:55 +0530

    changes in serial comm

--- Serial_comm_temp/mega_4/mega_4.ino
+++ Serial_comm_temp/mega_4/mega_4.ino
@@ -6,10 +6,8 @@ Serial2.begin(115200);
 }
 
 void loop() {
-//if(Serial2.available()>0);
-//    if(Serial2.read()==F){Serial.println();}
-//     Serial.print(Serial2.read());
-if((micros()-timer)>1000000){
+
+if((micros()-timer)>200000){
   Serial2.write('G');
   timer = micros();
 }
--- Serial_comm_temp/nano_5/nano_5.ino
+++ Serial_comm_temp/nano_5/nano_5.ino
@@ -1,19 +1,24 @@
 #include <SoftwareSerial.h>
 SoftwareSerial softSerial(3,4);
+int p1 = 2;
 float a=50.2;
 String b = "abc";
-unsigned long timer = 0;
+volatile unsigned long timer = 0 , duration = 0 ;
 
 void setup() {
   Serial.begin(115200);
   softSerial.begin(115200);
+  pinMode(p1,INPUT_PULLUP);
 }
 
 void loop() {
+  duration = pulseIn(p1,HIGH);
+  Serial.println(duration);
+
   
   if(softSerial.available()){
   if(softSerial.read()=='G'){
-  softSerial.print(a);
+  softSerial.print(duration);
   softSerial.write('F'); 
   }
   }
