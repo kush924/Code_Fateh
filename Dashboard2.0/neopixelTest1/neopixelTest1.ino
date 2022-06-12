742e70d66ee54c489fcad65b1d2a11481b6a9f71
kush924 <kush924@gmail.com>
2022-06-12 09:40:09 +0530

    neopixelTest1

--- Dashboard2.0/neopixelTest1/neopixelTest1.ino
+++ Dashboard2.0/neopixelTest1/neopixelTest1.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <FastLED.h>
 
+int LED_pin = 2;
+int LED_num = 19;
+
+CRGB leds[LED_num];
+
+void setup{
+    FastLED.addLeds<WS2812, LED_PIN, RGB>(leds, NUM_LEDS);
+    FastLED.setMaxPowerInVoltsAndMilliamps(5,500);
+    FastLED.clear();
+    FastLED.show();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop{
+  leds[0] = CRGB(255,0,0);
+  leds[0] = CRGB(0,255,0);
+  leds[0] = CRGB(0,0,255);
 
+  FastLED.show();
 }
